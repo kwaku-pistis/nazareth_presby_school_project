700aa2d6f4ff539dd14d86c3241a126d52c65340
Pistis <danielpartey.dp@gmail.com>
2021-08-09 09:05:34 +0000

    add header bamner to school project page

--- lib/screens/school_project.dart
+++ lib/screens/school_project.dart
@@ -23,152 +23,181 @@ class _SchoolProjectState extends State<SchoolProject> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        key: schoolProjectKey,
-        width: MediaQuery.of(context).size.width * 0.6,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-                margin: const EdgeInsets.only(top: 30),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                    color: CustomColor.red,
-                  ),
-                  child: const Text(
-                    'ABOUT PROJECT',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                    textAlign: TextAlign.left,
-                  ),
-                )),
-            Container(
-              // width: MediaQuery.of(context).size.width * 0.5,
-              margin: const EdgeInsets.only(top: 30),
+    return SizedBox(
+      key: schoolProjectKey,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 150,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/images/school.jpg'),
+              fit: BoxFit.cover,
+            )),
+            child: const Center(
               child: Text(
-                bodyText,
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Container(
-                margin: const EdgeInsets.only(bottom: 30),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                    color: CustomColor.red,
-                  ),
-                  child: const Text(
-                    'BUILDING PLAN',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                    textAlign: TextAlign.left,
-                  ),
-                )),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.6,
-              height: 400.0,
-              margin: const EdgeInsets.only(bottom: 10),
-              child: const Carousel(),
-            ),
-            Container(
-              alignment: Alignment.centerRight,
-              child: Text(
-                'Designed by: Mr. Hello World',
-                style: Theme.of(context).textTheme.headline3,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Container(
-                margin: const EdgeInsets.only(bottom: 20),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                    color: CustomColor.red,
-                  ),
-                  child: const Text(
-                    'GALLERY',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                    textAlign: TextAlign.left,
-                  ),
-                )),
-            Container(
-              padding: const EdgeInsets.only(bottom: 5),
-              decoration: const BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(
-                          color: CustomColor.red,
-                          width: 2,
-                          style: BorderStyle.solid))),
-              child: const Text(
-                'Before',
+                'SCHOOL BUILDING PROJECT',
                 style: TextStyle(
+                  fontSize: 24,
                   fontFamily: 'Raleway',
-                  fontSize: 16.0,
-                  color: CustomColor.blue,
                   fontWeight: FontWeight.bold,
-                ),
+                  color: CustomColor.white
+                )
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
+            )
+          ),
+          Center(
+            child: SizedBox(
               width: MediaQuery.of(context).size.width * 0.6,
-              height: 400.0,
-              margin: const EdgeInsets.only(bottom: 20),
-              child: const Carousel(),
-            ),
-            Container(
-              padding: const EdgeInsets.only(bottom: 5),
-              // alignment: Alignment.centerRight,
-              decoration: const BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      margin: const EdgeInsets.only(top: 30),
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
                           color: CustomColor.red,
-                          width: 2,
-                          style: BorderStyle.solid))),
-              child: const Text(
-                'On-going',
-                style: TextStyle(
-                  fontFamily: 'Raleway',
-                  fontSize: 16.0,
-                  color: CustomColor.blue,
-                  fontWeight: FontWeight.bold,
-                ),
+                        ),
+                        child: const Text(
+                          'ABOUT PROJECT',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      )),
+                  Container(
+                    // width: MediaQuery.of(context).size.width * 0.5,
+                    margin: const EdgeInsets.only(top: 30),
+                    child: Text(
+                      bodyText,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(bottom: 30),
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          color: CustomColor.red,
+                        ),
+                        child: const Text(
+                          'BUILDING PLAN',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      )),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    height: 400.0,
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: const Carousel(),
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Designed by: Mr. Hello World',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          color: CustomColor.red,
+                        ),
+                        child: const Text(
+                          'GALLERY',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      )),
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    decoration: const BoxDecoration(
+                        border: Border(
+                            bottom: BorderSide(
+                                color: CustomColor.red,
+                                width: 2,
+                                style: BorderStyle.solid))),
+                    child: const Text(
+                      'Before',
+                      style: TextStyle(
+                        fontFamily: 'Raleway',
+                        fontSize: 16.0,
+                        color: CustomColor.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    height: 400.0,
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: const Carousel(),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    // alignment: Alignment.centerRight,
+                    decoration: const BoxDecoration(
+                        border: Border(
+                            bottom: BorderSide(
+                                color: CustomColor.red,
+                                width: 2,
+                                style: BorderStyle.solid))),
+                    child: const Text(
+                      'On-going',
+                      style: TextStyle(
+                        fontFamily: 'Raleway',
+                        fontSize: 16.0,
+                        color: CustomColor.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    height: 400.0,
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: const Carousel(),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.6,
-              height: 400.0,
-              margin: const EdgeInsets.only(bottom: 10),
-              child: const Carousel(),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
